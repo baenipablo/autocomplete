d91aba59ecb1faab108111f62cebd1395f1dca6e
Wolfgang Bock <wolfgang.bock@htwchur.ch>
2019-07-04 11:26:32 +0200

    deleted DB-Data

--- server_scripts/database.php
+++ server_scripts/database.php
@@ -1,6 +1,6 @@
 <?php
 function get_db_connection(){ 
-    $db = new mysqli('localhost','177301_12_1','6qLRmKP1lVJY','177301_12_1'); 
+    $db = new mysqli('localhost','','',''); 
     $db->query("SET NAMES 'utf8'");
     return $db;
 }
